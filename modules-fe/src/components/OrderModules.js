b5dbb7d22045d1c7379b119759120c58d950db7c
Frank Filius <f.filius@amsterdam.nl>
2022-08-08 12:45:53 +0200

    Implement library to reorder

--- modules-fe/src/components/OrderModules.js
+++ modules-fe/src/components/OrderModules.js
@@ -1,72 +1,103 @@
-import styled from 'styled-components'
-import { Link } from 'react-router-dom'
-import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd'
-
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+import {
+    closestCenter,
+    DndContext,
+    KeyboardSensor,
+    PointerSensor,
+    useDraggable,
+    useSensor,
+    useSensors
+} from '@dnd-kit/core';
+import { useState } from 'react';
+import {
+    arrayMove,
+    SortableContext,
+    sortableKeyboardCoordinates,
+    useSortable,
+    verticalListSortingStrategy
+} from '@dnd-kit/sortable';
+import { CSS } from '@dnd-kit/utilities';
 
 const Container = styled.div`
     margin: 15px;
 `;
 
-const ModuleList = styled.ul`
-    padding: 8px;
+const StyledModuleList = styled.ul`
+    padding: '18px 0';
 `;
 
-const Module = (props) => {
-    return (
-        <Draggable key={props.module.slug} draggableId={props.module.slug} index={props.index}>
-            {(provided) => (
-                <li
-                    ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}
-                    style={{
-                        color: 'black',
-                        marginTop: 15,
-                        width: 324,
-                        display: 'flex',
-                        justifyContent: 'space-between'
-                    }}>
-                    {props.module.title}
-                    <div>
-                        <Link to={`/edit-module/${props.appversion}/${props.module.slug}/${props.module.version}`}>
-                            <img
-                                style={{
-                                    position: 'absolute',
-                                    right: '5px',
-                                    marginTop: '3px',
-                                }}
-                                height={'20px'}
-                                src='/edit-blue.svg'
-                                alt='Bewerk module' />
-                        </Link>
-                    </div>
-                </li>
-            )}
-        </Draggable>
-    )
-}
+const Module = ({ appversion, id, module }) => {
+    const { attributes, listeners, setNodeRef, transform, transition } = useSortable({ id });
 
-const OrderModules = (props) => {
+    const style = {
+        color: 'black',
+        marginTop: 15,
+        width: 324,
+        display: 'flex',
+        justifyContent: 'space-between',
+        transform: CSS.Transform.toString(transform),
+        transition
+    };
+    return (
+        <li style={style} ref={setNodeRef} {...attributes} {...listeners}>
+            {module.title}
+            <Link to={`/edit-module/${appversion}/${module.slug}/${module.version}`}>
+                <img
+                    style={{
+                        position: 'absolute',
+                        right: '5px',
+                        marginTop: '3px'
+                    }}
+                    height={'20px'}
+                    src="/edit-blue.svg"
+                    alt="Bewerk module"
+                />
+            </Link>
+        </li>
+    );
+};
+
+const ModuleList = ({ appversion, list }) => (
+    <StyledModuleList>
+        {list.map(module => (
+            <Module appversion={appversion} id={module.slug} key={module.slug} module={module} />
+        ))}
+    </StyledModuleList>
+);
+
+const OrderModules = ({ appversion, modules, setModules }) => {
+    const sensors = useSensors(
+        useSensor(PointerSensor),
+        useSensor(KeyboardSensor, {
+            coordinateGetter: sortableKeyboardCoordinates
+        })
+    );
+
+    const handleDragEnd = event => {
+        const { active, over } = event;
+
+        if (active.id !== over.id) {
+            setModules(modules => {
+                const moduleActive = modules.find(m => m.slug === active.id);
+                const moduleOver = modules.find(m => m.slug === over.id);
+                const oldIndex = modules.indexOf(moduleActive);
+                const newIndex = modules.indexOf(moduleOver);
+
+                return arrayMove(modules, oldIndex, newIndex);
+            });
+        }
+    };
+
     return (
         <Container>
-            <DragDropContext>
-                <Droppable droppableId="moduleOrder">
-                    {(provided) => (
-                        <ModuleList id='moduleOrder' className='moduleOrder' {...provided.droppableProps} ref={provided.innerRef}>
-                            {props.modules.map((module, index) => {
-                                return (
-                                    <Module
-                                        key={module.slug}
-                                        index={index}
-                                        appversion={props.appversion}
-                                        module={module} />
-                                );
-                            })}
-                            {provided.placeholder}
-                        </ModuleList>
-                    )}
-                </Droppable>
-            </DragDropContext>
+            <DndContext sensors={sensors} collisionDetection={closestCenter} onDragEnd={handleDragEnd}>
+                <SortableContext items={modules} strategy={verticalListSortingStrategy}>
+                    <ModuleList appversion={appversion} list={modules} />
+                </SortableContext>
+            </DndContext>
         </Container>
-    )
-}
+    );
+};
 
-export default OrderModules
+export default OrderModules;
--- modules-fe/src/pages/EditModules.js
+++ modules-fe/src/pages/EditModules.js
@@ -1,11 +1,10 @@
-import Logo from "../components/Logo"
-import Navigation from "../components/Navigation"
+import Logo from '../components/Logo'
+import Navigation from '../components/Navigation'
 import PageTitle from '../components/PageTitle'
 import useAPICalls from '../components/useAPICalls'
-import { useParams } from "react-router-dom"
+import { useParams } from 'react-router-dom'
 import { useEffect, useRef, useState } from 'react'
-import OrderModules from "../components/OrderModules"
-
+import OrderModules from '../components/OrderModules'
 
 const EditModules = () => {
     const { appversion } = useParams()
@@ -34,15 +33,15 @@ const EditModules = () => {
             setTimeout(() => {
                 setLog('')
                 newLog.current = false
-            }, 3000);
+            }, 3000)
         }
-    }, [log]);
+    }, [log])
 
     const saveOrder = async () => {
-        const order = modulesForApp.map((module) => { return module.slug })
+        const order = modulesForApp.map(module => module.slug)
         let payload = {
-            "appVersion": appversion,
-            "order": order
+            appVersion: appversion,
+            order: order
         }
 
         const { data, response } = await patchMethod('modules_order', {}, payload)
@@ -59,41 +58,43 @@ const EditModules = () => {
             <Logo />
 
             {/* Navigation */}
-            <Navigation location='/' text='Terug' />
+            <Navigation location="/" text="Terug" />
 
             {/* Title component */}
-            <PageTitle pageTitle='Bewerk modules' />
+            <PageTitle pageTitle="Bewerk modules" />
 
             <div className="page_body">
-                <div style={{
-                    color: 'black',
-                    marginLeft: '15px',
-                    height: '50px',
-                    display: 'flex',
-                    alignItems: 'center',
-                    maxWidth: '344px'
-                }}>
+                <div
+                    style={{
+                        color: 'black',
+                        marginLeft: '15px',
+                        height: '50px',
+                        display: 'flex',
+                        alignItems: 'center',
+                        maxWidth: '344px'
+                    }}
+                >
                     Pas de volgorde aan door de modules te verslepen of wijzig de naam
                 </div>
 
                 {/* Modules for this appVersion */}
-                <OrderModules appversion={appversion} modules={modulesForApp} />
+                <OrderModules appversion={appversion} modules={modulesForApp} setModules={setModulesForApp} />
 
-                <div style={{
-                    maxWidth: 144,
-                    color: 'red',
-                    marginLeft: '15px',
-                    height: '50px',
-                    display: 'flex',
-                    alignItems: 'center'
-                }}>
+                <div
+                    style={{
+                        maxWidth: 144,
+                        color: 'red',
+                        marginLeft: '15px',
+                        height: '50px',
+                        display: 'flex',
+                        alignItems: 'center'
+                    }}
+                >
                     {log}
                 </div>
 
-                <button
-                    className='saveButton'
-                    onClick={saveOrder}>
-                    <span className='submitlogintext'>Bewaar wijzigingen</span>
+                <button className="saveButton" onClick={saveOrder}>
+                    <span className="submitlogintext">Bewaar wijzigingen</span>
                 </button>
             </div>
         </div>
