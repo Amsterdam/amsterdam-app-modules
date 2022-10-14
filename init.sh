bdf22d3f5fdda4b0fcb57a48e3dc8bc4b8faf8c2
Jos Westerbeek <jos.westerbeek@amsterdam.nl>
2022-10-14 14:05:58 +0200

    Keep pytest out of infinity loop. Merge requirements.txt with requirements-pact.txt

--- init.sh
+++ init.sh
@@ -5,26 +5,26 @@ function header {
 }
 
 function SERVER {
-  if [ -z ${UNITTEST} ]; then
-    printf "Starting Module\n\n"
-    cd /code && . venv/bin/activate && python3 /code/main.py
-  else
-    printf "Starting unittests\n\n"
-    cd /code pytest --no-header --no-summary -q unittests/
-  fi  
+  cd /code && . venv/bin/activate && python3 /code/main.py  
 }
 
 function infinity_loop {
   # Automatically restart the flask-services on crash
   export PYTHONPATH=/code
-  while true; do
-    # For debugging: Touch /flask_app/DEBUG, kill run.py and restart manually
-    if [[ ! -f "/code/DEBUG" ]]
-    then
-	    SERVER;
-    fi
-    sleep 1
-  done
+  if [ -z ${UNITTEST} ]; then
+    while true; do
+      # For debugging: Touch /flask_app/DEBUG, kill run.py and restart manually
+      if [[ ! -f "/code/DEBUG" ]]
+      then
+        SERVER;
+      fi
+      sleep 1
+    done
+  else
+    printf "Starting unittests\n\n"
+    cd /code pytest --no-header --no-summary -q unittests/
+  fi  
+
 }
 
 header
