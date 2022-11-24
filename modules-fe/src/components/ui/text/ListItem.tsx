19be1f6059abb63ef22a6cf93ce1a59899ab4d74
Frank Filius <f.filius@amsterdam.nl>
2022-11-24 12:50:31 +0100

    Move styling from list to list item component

--- modules-fe/src/components/ui/text/ListItem.tsx
+++ modules-fe/src/components/ui/text/ListItem.tsx
@@ -1,9 +1,10 @@
 import { ReactNode } from 'react';
+import './ListItem.css';
 
 type Props = {
     children: ReactNode;
 };
 
-const ListItem = ({ children }: Props) => <li>{children}</li>;
+const ListItem = ({ children }: Props) => <li className="ListItem">{children}</li>;
 
 export default ListItem;
