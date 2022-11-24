2a261fef39a5abb8ae2c073fa723ad111b5e0347
Frank Filius <f.filius@amsterdam.nl>
2022-11-24 08:27:00 +0100

    Properly define mock data

--- modules-fe/src/pages/Modules.tsx
+++ modules-fe/src/pages/Modules.tsx
@@ -1,77 +1,41 @@
 import { Module } from '../types/module';
 
-const modules: Module[] = [
+const modules: Pick<Module, 'slug' | 'title'>[] = [
     {
-        description: 'Blijf op de hoogte van werkzaamheden in uw buurt.',
-        icon: 'projects',
         slug: 'construction-work',
-        status: 1,
-        title: 'Werkzaamheden',
-        version: '1.0.0'
+        title: 'Werkzaamheden'
     },
     {
-        description: 'Ziet u iets wat stuk is of opgeruimd moet worden?',
-        icon: 'alert',
         slug: 'report-problem',
-        status: 1,
-        title: 'Melding doen',
-        version: '1.0.0'
+        title: 'Melding doen'
     },
     {
-        description: 'Heeft u een vraag of wilt u iets weten?',
-        icon: 'chatting',
         slug: 'contact',
-        status: 1,
-        title: 'Contact',
-        version: '1.0.0'
+        title: 'Contact'
     },
     {
-        description: 'Op de website vindt u alle informatie die nog niet in de app zit.',
-        icon: 'checkmark',
         slug: 'redirects',
-        status: 1,
-        title: 'Direct regelen',
-        version: '1.0.0'
+        title: 'Direct regelen'
     },
     {
-        description: 'Lees meer over deze app en help ons de app verbeteren.',
-        icon: 'info',
         slug: 'about',
-        status: 1,
-        title: 'Over deze app',
-        version: '1.0.0'
+        title: 'Over deze app'
     },
     {
-        description: 'Bekijk een mooi Amsterdams beeld bij het starten van de app',
-        icon: 'housing',
         slug: 'welcome',
-        status: 1,
-        title: 'Welkomstscherm',
-        version: '1.0.0'
+        title: 'Welkomstscherm'
     },
     {
-        description: 'Bekijk waar afval naartoe kan en wat de ophaaldagen zijn.',
-        icon: 'trash-bin',
         slug: 'waste-guide',
-        status: 1,
-        title: 'Afvalwijzer',
-        version: '1.0.0'
+        title: 'Afvalwijzer'
     },
     {
-        description: 'Hier kun je als omgevingsmanager berichten plaatsen bij werkzaamheden',
-        icon: 'announcement',
         slug: 'construction-work-editor',
-        status: 1,
-        title: 'Plaats berichten',
-        version: '1.0.0'
+        title: 'Plaats berichten'
     },
     {
-        description: 'Open een container met de app in plaats van een plastic pas.',
-        icon: 'organic-waste-container',
         slug: 'open-waste-container',
-        status: 1,
-        title: 'Gft-container openen',
-        version: '1.0.0'
+        title: 'Gft-container openen'
     }
 ];
 
