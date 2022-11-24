a1744d394f9fd63798913a543d541a1a3413a8c1
Frank Filius <f.filius@amsterdam.nl>
2022-11-24 11:58:48 +0100

    Introduce row component

--- modules-fe/src/components/ui/layout/Row.tsx
+++ modules-fe/src/components/ui/layout/Row.tsx
@@ -1,7 +1,18 @@
+import { ReactNode } from 'react';
 import './Row.css';
+import { SpacingToken } from './types';
 
-const Row = () => {
-    return <div className="Row" />;
+type Props = {
+    children: ReactNode;
+    gutter?: keyof typeof SpacingToken;
+};
+
+const Row = ({ children, gutter }: Props) => {
+    return (
+        <div className="Row" data-gutter={gutter}>
+            {children}
+        </div>
+    );
 };
 
 export default Row;
