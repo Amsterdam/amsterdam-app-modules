c445e304faba3f0ec04d86a24d7f897856bb2b68
Frank Filius <f.filius@amsterdam.nl>
2022-11-24 08:27:49 +0100

    Install rtk query and create base api

--- modules-fe/src/App.tsx
+++ modules-fe/src/App.tsx
@@ -1,12 +1,16 @@
+import { Provider } from 'react-redux';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import ModulesPage from './pages/Modules';
+import { store } from './store/store';
 
 const App = () => (
-    <BrowserRouter>
-        <Routes>
-            <Route path="/" element={<ModulesPage />} />
-        </Routes>
-    </BrowserRouter>
+    <Provider store={store}>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<ModulesPage />} />
+            </Routes>
+        </BrowserRouter>
+    </Provider>
 );
 
 export default App;
