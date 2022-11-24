c22d8180d50c494518c54b3c66a2f7285904e34b
Vincent Smedinga <vincent@smedinga.nl>
2022-11-24 17:01:16 +0100

    Implement Box component

--- modules-fe/src/components/ui/layout/Box.tsx
+++ modules-fe/src/components/ui/layout/Box.tsx
@@ -1,3 +1,19 @@
-const Box = () => <h1>Box</h1>
+import {ReactNode} from 'react'
+import {SpacingToken} from './types'
+import './inset.css'
+
+type Props = {
+  children: ReactNode
+  /**
+   * The amount of inner whitespace.
+   */
+  inset?: keyof typeof SpacingToken
+}
+
+const Box = ({children, inset = 'md'}: Props) => (
+  <div className="Box" data-inset={inset}>
+    {children}
+  </div>
+)
 
 export default Box
