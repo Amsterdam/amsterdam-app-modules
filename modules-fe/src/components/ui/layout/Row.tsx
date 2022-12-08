9ea30705916597dfc8df181b86dfd472fe099e27
Frank Filius <f.filius@amsterdam.nl>
2022-12-08 14:44:32 +0100

    Add alignment property to row component

--- modules-fe/src/components/ui/layout/Row.tsx
+++ modules-fe/src/components/ui/layout/Row.tsx
@@ -1,17 +1,22 @@
 import {ReactNode} from 'react'
 import './Row.css'
 import './gutter.css'
-import {CrossAxisAlignment, SpacingToken} from './types'
+import {CrossAxisAlignment, MainAxisAlignment, SpacingToken} from './types'
 
 type Props = {
+  align?: MainAxisAlignment
   children: ReactNode
   gutter?: keyof typeof SpacingToken
   valign?: CrossAxisAlignment
 }
 
-const Row = ({children, gutter, valign}: Props) => {
+const Row = ({align, children, gutter, valign}: Props) => {
   return (
-    <div className="Row" data-gutter={gutter} data-valign={valign}>
+    <div
+      className="Row"
+      data-gutter={gutter}
+      data-align={align}
+      data-valign={valign}>
       {children}
     </div>
   )
