7f1aa6884e86e5e8ec19492587c3ea827d61cdb9
Frank Filius <f.filius@amsterdam.nl>
2022-12-12 13:34:35 +0100

    Add more input fields to form

--- modules-fe/src/screens/CreateModuleVersion.screen.tsx
+++ modules-fe/src/screens/CreateModuleVersion.screen.tsx
@@ -3,20 +3,25 @@ import {useCallback} from 'react'
 import {useForm, FormProvider, SubmitHandler} from 'react-hook-form'
 import {useParams} from 'react-router-dom'
 import Input from 'components/ui/forms/Input'
+import {Module} from 'types/module'
 import Box from '../components/ui/layout/Box'
 import Column from '../components/ui/layout/Column'
 import Screen from '../components/ui/layout/Screen'
+import {iconNames} from '../components/ui/media/iconPath'
 import Title from '../components/ui/text/Title'
 
 const CreateModuleVersionScreen = () => {
   const {slug} = useParams()
-  const form = useForm<FormData>()
+  const form = useForm<Module>()
 
   const {handleSubmit} = form
 
-  const onSubmitForm: SubmitHandler<FormData> = useCallback(data => {
-    console.log(data)
-  }, [])
+  const onSubmitForm: SubmitHandler<Module> = useCallback(
+    data => {
+      console.log({...data, slug})
+    },
+    [slug],
+  )
 
   return (
     <Screen>
@@ -32,6 +37,22 @@ const CreateModuleVersionScreen = () => {
                 name="title"
                 rules={{
                   required: 'Vul een titel in',
+                }}
+              />
+              <Input
+                label="Omschrijving"
+                name="description"
+                rules={{
+                  required: 'Vul een omschrijving in',
+                }}
+              />
+              <Input
+                label="Pictogram"
+                name="icon"
+                rules={{
+                  required: 'Vul een pictogram in',
+                  validate: value =>
+                    iconNames.includes(value) || 'Kies een bestaand pictogram',
                 }}
               />
               <button onClick={handleSubmit(onSubmitForm)} type="submit">
