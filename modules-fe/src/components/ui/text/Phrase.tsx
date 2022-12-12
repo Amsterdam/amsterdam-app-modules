5a70eb7752834510351bd436106f2e133c3389a2
Frank Filius <f.filius@amsterdam.nl>
2022-12-12 13:32:58 +0100

    Enable choosing a color for phrase component

--- modules-fe/src/components/ui/text/Phrase.tsx
+++ modules-fe/src/components/ui/text/Phrase.tsx
@@ -3,8 +3,13 @@ import './Phrase.css'
 
 type Props = {
   children: ReactNode
+  color?: 'error' | 'default'
 }
 
-const Phrase = ({children}: Props) => <span className="Phrase">{children}</span>
+const Phrase = ({children, color = 'default'}: Props) => (
+  <span className="Phrase" data-color={color}>
+    {children}
+  </span>
+)
 
 export default Phrase
