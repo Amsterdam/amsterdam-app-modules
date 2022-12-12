7d8138181aeb3e17ce6b933d3e1c53029b5e9a21
Vincent Smedinga <vincent@smedinga.nl>
2022-12-12 21:12:36 +0100

    Implement design for inputs

--- modules-fe/src/components/ui/forms/Input.tsx
+++ modules-fe/src/components/ui/forms/Input.tsx
@@ -1,4 +1,5 @@
 import {Controller, UseControllerProps} from 'react-hook-form'
+import './Input.css'
 import Column from '../layout/Column'
 import Phrase from '../text/Phrase'
 import Label from './Label'
@@ -15,7 +16,7 @@ const Input = ({defaultValue = '', label, name, rules}: Props) => {
       render={({field: {onChange, value}, fieldState: {error}}) => (
         <Column gutter="sm">
           <Label text={label} />
-          <input onChange={onChange} value={value} />
+          <input className="Input" onChange={onChange} value={value} />
           {!!error && <Phrase color="error">{error.message}</Phrase>}
         </Column>
       )}
