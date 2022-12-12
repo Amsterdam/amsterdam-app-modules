edea9cca74e1def5da65ee60c7bad76fb78ffae3
Vincent Smedinga <vincent@smedinga.nl>
2022-12-12 21:21:34 +0100

    Extract local component for individual radio button

--- modules-fe/src/components/ui/forms/RadioGroup.tsx
+++ modules-fe/src/components/ui/forms/RadioGroup.tsx
@@ -4,41 +4,52 @@ import Row from '../layout/Row'
 import Phrase from '../text/Phrase'
 import Label from './Label'
 
-type Props = {
+type RadioProps = {
+  isLast: boolean
+  option: string
+} & UseControllerProps
+
+const Radio = ({isLast, name, option, rules}: RadioProps) => (
+  <Controller
+    key={option}
+    name={name}
+    render={({field: {onChange}, fieldState: {error}}) => {
+      return (
+        <Column gutter="sm" halign="start">
+          <label htmlFor={`version-${option}`}>
+            <Row gutter="sm" valign="center">
+              <input
+                id={`version-${option}`}
+                name="version"
+                onChange={onChange}
+                type="radio"
+                value={option}
+              />
+              <Phrase>{option}</Phrase>
+            </Row>
+          </label>
+          {isLast && !!error && <Phrase color="error">{error.message}</Phrase>}
+        </Column>
+      )
+    }}
+    rules={rules}
+  />
+)
+
+type RadioGroupProps = {
   label: string
   options: string[]
 } & UseControllerProps
 
-const RadioGroup = ({label, name, options, rules}: Props) => (
+const RadioGroup = ({label, name, options, rules}: RadioGroupProps) => (
   <Column gutter="sm">
     <Label text={label} />
     <Column>
       {options.map((option, index) => (
-        <Controller
+        <Radio
+          isLast={index === options.length - 1}
           key={option}
-          name={name}
-          render={({field: {onChange}, fieldState: {error}}) => {
-            return (
-              <Column gutter="sm" halign="start">
-                <label htmlFor={`version-${option}`}>
-                  <Row gutter="sm" valign="center">
-                    <input
-                      id={`version-${option}`}
-                      name="version"
-                      onChange={onChange}
-                      type="radio"
-                      value={option}
-                    />
-                    <Phrase>{option}</Phrase>
-                  </Row>
-                </label>
-                {!!error && index === options.length - 1 && (
-                  <Phrase color="error">{error.message}</Phrase>
-                )}
-              </Column>
-            )
-          }}
-          rules={rules}
+          {...{name, option, rules}}
         />
       ))}
     </Column>
