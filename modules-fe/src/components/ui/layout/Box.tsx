21cb1fa184a5c909e438c39efd5ccda2158fff65
Vincent Smedinga <vincent@smedinga.nl>
2022-12-13 13:08:44 +0100

    Introduce horizontal and vertical inset props for `Box`

--- modules-fe/src/components/ui/layout/Box.tsx
+++ modules-fe/src/components/ui/layout/Box.tsx
@@ -8,10 +8,27 @@ type Props = {
    * The amount of inner whitespace.
    */
   inset?: keyof typeof SpacingToken
+  /**
+   * The amount of horizontal inner whitespace.
+   */
+  insetHorizontal?: keyof typeof SpacingToken
+  /**
+   * The amount of vertical inner whitespace.
+   */
+  insetVertical?: keyof typeof SpacingToken
 }
 
-const Box = ({children, inset = 'md'}: Props) => (
-  <div className="Box" data-inset={inset}>
+const Box = ({
+  children,
+  inset = 'md',
+  insetHorizontal,
+  insetVertical,
+}: Props) => (
+  <div
+    className="Box"
+    data-inset={inset}
+    data-inset-horizontal={insetHorizontal}
+    data-inset-vertical={insetVertical}>
     {children}
   </div>
 )
