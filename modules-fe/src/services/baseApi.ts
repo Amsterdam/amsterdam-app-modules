d56a975029817e538d53afa8dab639ceafa2b92e
Frank Filius <f.filius@amsterdam.nl>
2022-12-13 12:56:45 +0100

    Set authorization header to create module endpoint

--- modules-fe/src/services/baseApi.ts
+++ modules-fe/src/services/baseApi.ts
@@ -1,6 +1,6 @@
 import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'
 
-const authorizedEndpoints = ['']
+const authorizedEndpoints = ['createModule']
 
 const mockUniqueId = 'mock-unique-id'
 
