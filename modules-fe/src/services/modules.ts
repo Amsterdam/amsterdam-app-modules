4e8ee4dd769fb25f3f2bee86db4948bc4285fcec
Frank Filius <f.filius@amsterdam.nl>
2022-12-13 10:18:42 +0100

    Create endpoint for creating a module

--- modules-fe/src/services/modules.ts
+++ modules-fe/src/services/modules.ts
@@ -5,23 +5,32 @@ type ModuleQueryArg = {
   slug: ModuleSlug
 }
 
-// TODO: Make `slug` a url part instead of a query param (BE)
-// TODO: Remove the `undefined` in the `getModules` query
 // TODO: Extract the `window.location.origin`
-// TODO: Sort module versions descending (BE)
 export const modulesApi = baseApi.injectEndpoints({
   endpoints: builder => ({
+    createModule: builder.mutation<Module, Module>({
+      query: module => ({
+        url: `${window.location.origin}/api/v1/modules`,
+        method: 'POST',
+        body: module,
+      }),
+      transformResponse: (response: {result: Module}) => response.result,
+      invalidatesTags: ['Module'],
+    }),
     getModule: builder.query<Module[], ModuleQueryArg>({
       query: ({slug}) =>
         `${window.location.origin}/api/v1/modules?slug=${slug}`,
       transformResponse: (response: {result: Module[]}) => response.result,
+      providesTags: ['Module'],
     }),
     getModules: builder.query<Module[], void>({
       query: () => `${window.location.origin}/api/v1/modules`,
       transformResponse: (response: {result: Module[]}) => response.result,
+      providesTags: ['Module'],
     }),
   }),
   overrideExisting: true,
 })
 
-export const {useGetModulesQuery, useGetModuleQuery} = modulesApi
+export const {useCreateModuleMutation, useGetModulesQuery, useGetModuleQuery} =
+  modulesApi
