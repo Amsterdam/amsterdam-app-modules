02cf09b225843393e0bbdd90577a67d26ce3a7d1
Frank Filius <f.filius@amsterdam.nl>
2022-12-13 11:24:20 +0100

    Set authorization header to certain endpoints

--- modules-fe/src/services/baseApi.ts
+++ modules-fe/src/services/baseApi.ts
@@ -1,7 +1,20 @@
 import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'
 
+const authorizedEndpoints = ['']
+
+const mockUniqueId = 'mock-unique-id'
+
 export const baseApi = createApi({
-  baseQuery: fetchBaseQuery({baseUrl: '/'}),
+  baseQuery: fetchBaseQuery({
+    baseUrl: '/',
+    prepareHeaders: (headers, {endpoint}) => {
+      if (authorizedEndpoints.includes(endpoint)) {
+        headers.set('Authorization', mockUniqueId)
+      }
+
+      return headers
+    },
+  }),
   endpoints: () => ({}),
   tagTypes: ['Module'],
 })
