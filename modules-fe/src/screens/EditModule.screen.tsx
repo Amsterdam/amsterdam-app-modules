1495916c384e6e6526fb788291f97d2225cd3f95
Vincent Smedinga <v.smedinga@amsterdam.nl>
2022-12-16 12:21:56 +0100

    Remove mock data and utils

--- modules-fe/src/screens/EditModule.screen.tsx
+++ modules-fe/src/screens/EditModule.screen.tsx
@@ -1,33 +1,23 @@
 import {useParams} from 'react-router-dom'
-import ErrorBox from '../components/ui/feedback/ErrorBox'
 import Column from '../components/ui/layout/Column'
 import Row from '../components/ui/layout/Row'
 import Screen from '../components/ui/layout/Screen'
 import Phrase from '../components/ui/text/Phrase'
 import Title from '../components/ui/text/Title'
-import {getModuleVersion} from '../services/modules.mock'
-import {ModuleSlug} from '../types/module'
 
 const EditModuleScreen = () => {
   const {slug, version} = useParams()
-  const module = getModuleVersion(slug as ModuleSlug, version)
-
-  if (!module) {
-    return <ErrorBox message="Geen moduleversie." />
-  }
 
   return (
     <Screen>
       <Column gutter="xl">
-        <Title>
-          Module bewerken: {module.title} {module.version}
-        </Title>
+        <Title>Module bewerken</Title>
         <Column gutter="md">
           <Row>
-            <Phrase>Naam: {module.title}</Phrase>
+            <Phrase>Slug: {slug}</Phrase>
           </Row>
           <Row>
-            <Phrase>Versie: {module.version}</Phrase>
+            <Phrase>Versie: {version}</Phrase>
           </Row>
         </Column>
       </Column>
