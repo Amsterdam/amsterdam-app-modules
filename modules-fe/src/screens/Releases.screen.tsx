c5ca95691e80f541109ba46b8392ea5f19aa6da2
Vincent Smedinga <v.smedinga@amsterdam.nl>
2022-12-18 23:23:15 +0100

    Add initial screen to list releases

--- modules-fe/src/screens/Releases.screen.tsx
+++ modules-fe/src/screens/Releases.screen.tsx
@@ -1,13 +1,43 @@
+import ErrorBox from '../components/ui/feedback/ErrorBox'
+import LoadingBox from '../components/ui/feedback/LoadingBox'
+import Box from '../components/ui/layout/Box'
 import Column from '../components/ui/layout/Column'
 import Screen from '../components/ui/layout/Screen'
+import List from '../components/ui/text/List'
+import ListItem from '../components/ui/text/ListItem'
+import Phrase from '../components/ui/text/Phrase'
 import Title from '../components/ui/text/Title'
+import {useGetReleasesQuery} from '../services/releases'
 
-const ReleasesScreen = () => (
-  <Screen>
-    <Column gutter="xl">
-      <Title>Releases</Title>
-    </Column>
-  </Screen>
-)
+const ReleasesScreen = () => {
+  const {data: releases, isLoading} = useGetReleasesQuery()
+
+  if (isLoading) {
+    return <LoadingBox />
+  }
+
+  if (!releases) {
+    return <ErrorBox message="Geen releases gevonden." />
+  }
+
+  return (
+    <Screen>
+      <Column gutter="lg">
+        <Title>Releases</Title>
+        <Box inset="no" negativeInsetHorizontal="md">
+          <List>
+            {releases.map(version => (
+              <ListItem key={version}>
+                <Box>
+                  <Phrase>Versie {version}</Phrase>
+                </Box>
+              </ListItem>
+            ))}
+          </List>
+        </Box>
+      </Column>
+    </Screen>
+  )
+}
 
 export default ReleasesScreen
