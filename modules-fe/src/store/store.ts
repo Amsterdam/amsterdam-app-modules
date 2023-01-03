c4ea9df2523d0827769eff864ca2eccacad4d90e
Frank Filius <f.filius@amsterdam.nl>
2023-01-03 12:54:57 +0100

    Create slice to manage release

--- modules-fe/src/store/store.ts
+++ modules-fe/src/store/store.ts
@@ -1,10 +1,12 @@
 import {configureStore} from '@reduxjs/toolkit'
 import {setupListeners} from '@reduxjs/toolkit/query'
 import {modulesApi} from '../services/modules'
+import {releaseSlice} from '../slices/release.slice'
 
 export const store = configureStore({
   reducer: {
     [modulesApi.reducerPath]: modulesApi.reducer,
+    release: releaseSlice.reducer,
   },
   middleware: getDefaultMiddleware =>
     getDefaultMiddleware().concat(modulesApi.middleware),
