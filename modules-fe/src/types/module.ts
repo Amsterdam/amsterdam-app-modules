bfc29ce67127bb68114c5ada69d66022773aaa6a
Frank Filius <f.filius@amsterdam.nl>
2023-01-11 16:40:20 +0100

    Separate module icons from others

--- modules-fe/src/types/module.ts
+++ modules-fe/src/types/module.ts
@@ -1,4 +1,4 @@
-import {IconName} from '../components/ui/media/iconPath'
+import {ModuleIconName} from '../components/ui/media/iconPath'
 
 export type Module = {
   slug: string
@@ -8,7 +8,7 @@ export type Module = {
 
 export type ModuleVersion = {
   description: string
-  icon: IconName
+  icon: ModuleIconName
   moduleSlug: string
   title: string
   version: string
