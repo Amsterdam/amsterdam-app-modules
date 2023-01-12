92b25bb609bb7ce63ab19ae2d121d69f27c2a07b
Vincent Smedinga <v.smedinga@amsterdam.nl>
2023-01-12 10:40:23 +0100

    Remove unnecessary svg properties

--- modules-fe/src/components/ui/forms/CheckboxField.tsx
+++ modules-fe/src/components/ui/forms/CheckboxField.tsx
@@ -22,11 +22,7 @@ const Indicator = ({isSelected}: IndicatorProps) => {
       {isSelected === 'indeterminate' ? (
         <line x1="5" y1="12" x2="19" y2="12" />
       ) : (
-        <path
-          fillRule="evenodd"
-          clipRule="evenodd"
-          d="M10.1079 17.5082L4 11.0841L5.45546 9.69887L10.128 14.6173L18.5646 6L20 7.40025L10.1079 17.5082Z"
-        />
+        <path d="M10.1079 17.5082L4 11.0841L5.45546 9.69887L10.128 14.6173L18.5646 6L20 7.40025L10.1079 17.5082Z" />
       )}
     </svg>
   )
