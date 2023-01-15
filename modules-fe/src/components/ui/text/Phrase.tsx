57734aab0846727f4dfaa9679540c189d4b9d7e5
Vincent Smedinga <v.smedinga@amsterdam.nl>
2023-01-15 23:07:28 +0100

    Allow bold phrases

--- modules-fe/src/components/ui/text/Phrase.tsx
+++ modules-fe/src/components/ui/text/Phrase.tsx
@@ -4,10 +4,15 @@ import './Phrase.css'
 type Props = {
   children: ReactNode
   color?: 'error' | 'inverse' | 'muted'
+  emphasis?: 'strong'
 }
 
-const Phrase = ({children, color}: Props) => (
-  <span className="Phrase" data-color={color} data-font="body">
+const Phrase = ({children, color, emphasis}: PhraseProps) => (
+  <span
+    className="Phrase"
+    data-color={color}
+    data-emphasis={emphasis}
+    data-font="body">
     {children}
   </span>
 )
