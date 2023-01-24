2d18fb8bd6f1681edd6a04465ab4301f5394ae5a
Frank Filius <f.filius@amsterdam.nl>
2023-01-24 13:40:34 +0100

    Make sure token is valid before setting auth header

--- modules-fe/src/services/baseApi.ts
+++ modules-fe/src/services/baseApi.ts
@@ -1,4 +1,16 @@
-import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'
+import {
+  BaseQueryFn,
+  createApi,
+  FetchArgs,
+  fetchBaseQuery,
+  FetchBaseQueryError,
+} from '@reduxjs/toolkit/query/react'
+import {
+  selectAuthorizationAccesstoken,
+  selectAuthorizationRefreshtoken,
+} from 'slices/authorization.slice'
+import {RootState} from 'store/store'
+import {getTokenValidity} from 'utils/authorization'
 
 const authorizedEndpoints = [
   'createModule',
@@ -7,19 +19,54 @@ const authorizedEndpoints = [
   'editModuleVersionStatus',
 ]
 
-const mockAuthorizationKey = 'mock-authorization-id'
+const baseQuery: BaseQueryFn<
+  string | FetchArgs,
+  unknown,
+  FetchBaseQueryError
+> = async (args, baseQueryApi, extraOptions) => {
+  const {dispatch, getState} = baseQueryApi
+  const getNewAccessToken = async () => {
+    const token = selectAuthorizationAccesstoken(getState() as RootState)
+    if (!getTokenValidity(token)) {
+      const refresh = selectAuthorizationRefreshtoken(getState() as RootState)
+      const refreshResult = await fetchBaseQuery({
+        baseUrl: '/',
+      })(
+        {
+          url: '/api/v1/token/refresh',
+          method: 'POST',
+          body: {refresh},
+        },
+        baseQueryApi,
+        {},
+      )
+      if (refreshResult.data) {
+        dispatch({
+          type: 'authorization/setTokens',
+          payload: refreshResult.data,
+        })
+      } else {
+        dispatch({type: 'authorization/logout'})
+      }
+    }
+  }
 
-export const baseApi = createApi({
-  baseQuery: fetchBaseQuery({
+  await getNewAccessToken()
+
+  return fetchBaseQuery({
     baseUrl: '/',
     prepareHeaders: (headers, {endpoint}) => {
+      const token = selectAuthorizationAccesstoken(getState() as RootState)
       if (authorizedEndpoints.includes(endpoint)) {
-        headers.set('Authorization', mockAuthorizationKey)
+        headers.set('Authorization', token)
       }
-
       return headers
     },
-  }),
+  })(args, baseQueryApi, extraOptions)
+}
+
+export const baseApi = createApi({
+  baseQuery,
   endpoints: () => ({}),
   tagTypes: ['Module', 'Release'],
 })
