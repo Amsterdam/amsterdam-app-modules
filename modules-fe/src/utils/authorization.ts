1539451c53bfa91b78bb602e06868b11fe7bacf6
Frank Filius <f.filius@amsterdam.nl>
2023-01-25 09:01:20 +0100

    Explicitly return boolean

--- modules-fe/src/utils/authorization.ts
+++ modules-fe/src/utils/authorization.ts
@@ -4,7 +4,7 @@ export const getTokenValidity = (token: string) => {
   if (token) {
     const {exp} = jwtDecode<JwtPayload>(token)
     const now = new Date().getTime() / 1000
-    return exp && exp > now
+    return !!exp && exp > now
   }
   return false
 }
